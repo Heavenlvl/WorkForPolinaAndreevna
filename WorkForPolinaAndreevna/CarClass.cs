ab6afc1acbd7f01de7c0a668ef8d54e81f006892
User <User@DESKTOP-HGVNFKQ>
2023-02-27 00:56:43 +0700

    Добавьте файлы проекта.

---
+++ WorkForPolinaAndreevna/CarClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkForPolinaAndreevna
+{
+    internal class CarClass
+    {
+        public string Mark;
+        public string Model;
+        public string Color;
+        public string Number;
+
+
+        public CarClass(string mark = "No mark", string model = "model", string color = "No color", string number = "No number")
+        {
+            this.Mark = mark;
+            this.Model = model;
+            this.Color = color;
+            this.Number = number;
+        }
+    }
+}
